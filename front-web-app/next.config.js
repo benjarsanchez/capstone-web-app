443da9e2c210e62fd4422c13b99cc33b51c50a57
BENJAMIN SANCHEZ <bsanchezc@uc.cl>
2023-05-10 11:22:18 -0400

    test url rewrite

--- front-web-app/next.config.js
+++ front-web-app/next.config.js
@@ -4,3 +4,14 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/auth/register",
+        destination: "http://15.223.120.64/auth/register",
+      },
+    ];
+  },
+};
